a8824748310bc2aee0604b64dc74321f6fa19bbb
newHYH <houge1422841169@qq.com>
2017-05-16 17:09:23 +0800

    install express and test express server

--- test.js
+++ test.js
@@ -2,7 +2,7 @@
 * @Author: hou
 * @Date:   2017-05-15 14:17:00
 * @Last Modified by:   anchen
-* @Last Modified time: 2017-05-15 14:29:55
+* @Last Modified time: 2017-05-16 17:08:23
 */
 // var http = require('http');
 // http.createServer(function (req,res){
@@ -10,9 +10,13 @@
 //     res.end('hello world\n');
 // }).listen(8081,'127.0.0.1');
 // console.log('Server is running at http://127.0.0.1:8081');
-var http = require('http');
-　　http.createServer(function (req, res) {
-　　  res.writeHead(200, {'Content-Type': 'text/plain'});
- 　　 res.end('Hello World\n');
-　　}).listen(3000, "127.0.0.1");
-　　console.log('Server running at http://127.0.0.1:3000/');
+/*express*/
+var express = require('express');
+var app = express();
+var port = 8082;
+var ip = "127.0.0.1";
+app.listen(port,ip);
+app.get('',function (req,res){
+    res.send("hello,world!\n");
+})
+console.log("start express server!");
