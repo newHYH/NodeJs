852f50c0445d12fdb19346b8e9a8c3677ff540ff
newHYH <houge1422841169@qq.com>
2017-05-16 17:14:13 +0800

    test express get function

--- test.js
+++ test.js
@@ -2,7 +2,7 @@
 * @Author: hou
 * @Date:   2017-05-15 14:17:00
 * @Last Modified by:   anchen
-* @Last Modified time: 2017-05-16 17:08:23
+* @Last Modified time: 2017-05-16 17:12:47
 */
 // var http = require('http');
 // http.createServer(function (req,res){
@@ -16,7 +16,16 @@ var app = express();
 var port = 8082;
 var ip = "127.0.0.1";
 app.listen(port,ip);
-app.get('',function (req,res){
-    res.send("hello,world!\n");
-})
+app.get('/',function (req,res){
+    res.send("this is default！\n");
+});
+app.get('/router1',function (req,res){
+    res.send("this is router1\n");
+});
+app.get('/router2',function (req,res){
+    res.send("this is router2\n");
+});
+app.get('/router3',function (req,res){
+    res.send("this is router3\n");
+});
 console.log("start express server!");
